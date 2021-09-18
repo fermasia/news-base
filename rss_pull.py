855f4975ca1bdd1c30044bb98037498792a0a4ac
NandoMasia <fermasia@hotmail.com>
2021-09-18 17:26:20 -0300

    Add files via upload

---
+++ rss_pull.py
@@ -0,0 +1,126 @@
+!pip install feedparser
+!pip install --upgrade gupload
+import html
+import json
+import requests
+from bs4 import BeautifulSoup
+import feedparser
+import webbrowser
+import re
+import pandas as pd
+from datetime import datetimeimpor
+from pydrive.auth import GoogleAuth
+from google.colab import auth
+from google.colab import files
+from google.colab import drive
+
+# Define Feeds in the form of a dict entry: media name, news category, rss url
+feeds = [{'source':'la_nacion','category':'politica','url':'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30'},\
+         {'source':'la_nacion','category':'mundo','url':' http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7'},\
+         {'source':'la_nacion','category':'economia','url':'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272'},\
+         {'source':'la_nacion','category':'sociedad','url':'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773'},\
+         {'source':'la_nacion','category':'opinion','url':'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=28'},\
+         {'source':'la_nacion','category':'portada','url':' http://contenidos.lanacion.com.ar/herramientas/rss/origen=1'},\
+         \
+         {'source':'pagina_12','category':'politica','url':'https://www.pagina12.com.ar/rss/secciones/el-pais/notas'},\
+         {'source':'pagina_12','category':'mundo','url':'https://www.pagina12.com.ar/rss/secciones/el-mundo/notas'},\
+         {'source':'pagina_12','category':'economia','url':'https://www.pagina12.com.ar/rss/secciones/economia/notas'},\
+         {'source':'pagina_12','category':'sociedad','url':'https://www.pagina12.com.ar/rss/secciones/sociedad/notas'},\
+         {'source':'pagina_12','category':'portada','url':'https://www.pagina12.com.ar/rss/portada'},\
+         \
+         {'source':'clarin','category':'politica','url':'https://www.clarin.com/rss/politica/'},\
+         {'source':'clarin','category':'mundo','url':'https://www.clarin.com/rss/mundo/'},\
+         {'source':'clarin','category':'economia','url':'https://www.clarin.com/rss/economia/'},\
+         {'source':'clarin','category':'sociedad','url':'https://www.clarin.com/rss/sociedad/'},\
+         {'source':'clarin','category':'opinion','url':'	https://www.clarin.com/rss/opinion/'},\
+        # \
+         #{'source':'ambito','category':'politica','url':'https://www.ambito.com/rss/politica.xml'},\
+         #{'source':'ambito','category':'mundo','url':'https://www.ambito.com/rss/mundo.xml'},\
+         #{'source':'ambito','category':'economia','url':'https://www.ambito.com/rss/economia.xml'},\
+         #{'source':'ambito','category':'sociedad','url':'https://www.ambito.com/rss/informacion-general.xml'},\
+         #{'source':'ambito','category':'opinion','url':'	https://www.ambito.com/rss/opinion.xml'}\
+        # \
+         #{'source':'perfil','category':'politica','url':'https://www.perfil.com/feed/politica'},\
+         #{'source':'perfil','category':'mundo','url':'https://www.perfil.com/feed/internacionales'},\
+         #{'source':'perfil','category':'economia','url':'https://www.perfil.com/feed/economia'},\
+         #{'source':'perfil','category':'sociedad','url':'https://www.perfil.com/feed/sociedad'},\
+         #{'source':'perfil','category':'opinion','url':'https://www.perfil.com/feed/opinion'}
+         ]
+
+# Initialize Destination DataFrame
+drive.mount('/content/drive')
+news_path = F"/content/drive/MyDrive/news/news.csv"
+df = pd.DataFrame(columns=['source','category','date','title','text','link'])
+#df = pd.read_csv(news_path,usecols=['source','category','date','title','text','link'])
+
+# Define Functions
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    clean_text = re.sub(clean, '', text)
+    unescaped_text = html.unescape(clean_text)
+    return unescaped_text
+
+def get_articlebody(link):
+  """ Receives an html link containing a news article and returns
+  a plain text string with the article body"""
+  soup = BeautifulSoup(requests.get(link).content, "html.parser")
+  json_data = json.loads(soup.find(type="application/ld+json").string)
+  return(json_data['description'])
+
+def get_news_tag(link):
+  """ Receives an html link containing a news article and finds the "news-content" tag
+  to retrieve a plain text string with the article body"""
+  try:
+    r = requests.get(link, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    container = soup.find('div', class_='news-content')
+    if container is None:
+      return float('Nan')
+    else:
+      for para in container.find_all('p', recursive=False):
+        return para.text
+  except ConnectionResetError:
+    return float('Nan')
+
+def get_news(rss):
+  """ Receives a dict entry containing media name, news category, rss url 
+  and parses de RSS feed extracting news date, title and content 
+  and append it to a destination pandas dataframe named df """
+  global df
+  data = feedparser.parse(rss['url'])
+  if rss['source'] in ['clarin','ambito']:
+    for entry in data.entries:
+      new_row = {'source':rss['source'],'category':rss['category'],'date':entry.published, 'title':entry.title, 'text':remove_html_tags(get_articlebody(entry.link)),'link':entry.link}
+      df = df.append(new_row, ignore_index=True)
+  elif rss['source'] == 'perfil':
+    for entry in data.entries:
+      new_row = {'source':rss['source'],'category':rss['category'],'date':entry.published, 'title':entry.title, 'text':get_news_tag(entry.link),'link':entry.link}
+      df = df.append(new_row, ignore_index=True)
+  else:
+    for entry in data.entries:
+      new_row = {'source':rss['source'],'category':rss['category'],'date':entry.published, 'title':entry.title, 'text':remove_html_tags(entry.content[0].value),'link':entry.link}
+      df = df.append(new_row, ignore_index=True)
+
+# Iterate RSS feeds list and append destination DF
+for feed in feeds:
+  get_news(feed)
+
+df.to_csv('test.csv')
+files.download('test.csv')
+
+df[100:101].link
+
+df.shape
+
+# Sanitize duplicate rows taking url as key
+df.drop_duplicates(subset='link', keep="first",inplace=True)
+
+# Store previous CSV as a backup with today's date and dump updated CSV as news.csv
+import os
+bk_filename = 'news_bk_' + datetime.today().strftime('%Y-%m-%d'+'.csv')
+bk_path = F"/content/drive/MyDrive/news/{bk_filename}" 
+os.rename("/content/drive/MyDrive/news/news.csv",bk_path)
+filename = 'news.csv'
+path = F"/content/drive/MyDrive/news/{filename}" 
+df.to_csv(path,index=False)
