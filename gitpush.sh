5f59b736c07bd71f15be5039e7d66fc9acd26b13
fermasia <fermasia@hotmail.com>
2021-10-10 16:08:20 +0000

    Commited Automatically

--- gitpush.sh
+++ gitpush.sh
@@ -4,4 +4,5 @@ conda activate base
 python /home/ec2-user/news-base/rss_pull.py
 git add .
 git commit -m "Commited Automatically"
+rm .git/hooks/pre-push #workaround for authentication issues with git-lfs
 git push
