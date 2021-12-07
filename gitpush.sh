837cbd6d7f71f81c26b2fb2d4c640307b048b1dc
EC2 Default User <ec2-user@ip-172-31-2-189.us-east-2.compute.internal>
2021-12-07 03:23:17 +0000

    rename sh file and simply run the python script

--- gitpush.sh
+++
@@ -1,9 +0,0 @@
-cd /home/ec2-user/news-base
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate base
-python /home/ec2-user/news-base/rss_pull.py
-git add .
-git commit -m "Commited Automatically"
-rm .git/hooks/pre-push #workaround for authentication issues with git-lfs
-git push
-aws s3 cp news.csv s3://newsbucketmas
---
+++ runcode.sh
@@ -0,0 +1,1 @@
+python3 /home/ec2-user/news-base/rss_pull.py
